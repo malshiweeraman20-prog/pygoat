39bf8fae98f97c368480c630cca54c96f1af0209
Malshi Weeraman <malshiweeraman20@gmail.com>
2026-09-25 05:03:10 -0400

    Configure PyGoat with PostgreSQL backend, fix Dockerfile for EOL Debian buster, resolve port conflicts

--- pygoat/settings.py
+++ pygoat/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,7 +29,6 @@ SENSITIVE_DATA = 'FLAGTHATNEEDSTOBEFOUND'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pygoat.herokuapp.com', '0.0.0.0.']
 
 
 # Application definition
@@ -89,8 +88,12 @@ WSGI_APPLICATION = 'pygoat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
@@ -144,7 +147,7 @@ LOGOUT_REDIRECT_URL = '/'
 
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 #Authentication Backend
 
